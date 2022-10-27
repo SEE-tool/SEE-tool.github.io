013fa4e09ebacbfb60b2fd9c98e9defea9912586
AnLau25 <avald024@uottawa.ca>
2022-10-26 22:51:33 -0400

    Starting Java Script, CSS still a problem

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-var a = 1;
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+})
+
