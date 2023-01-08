d0b1413da229ff565be334c9e3fcdd88830b572e
AnLau25 <avald024@uottawa.ca>
2023-01-07 19:33:53 -0500

    idk

--- main.js
+++ main.js
@@ -16,3 +16,9 @@ document.addEventListener('scroll',()=>{
     }
     });
 
+    menu_item.forEach((item) => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('active');
+            mobile_menu.classList.toggle('active');
+        });
+    });
